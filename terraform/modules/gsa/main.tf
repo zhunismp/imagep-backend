4f62b6975bdfe00585401f49ba70b9bfa4b18174
zhunismp <zhunismp@sensei.com>
2026-01-23 16:32:03 +0700

    feat: try hard argo

--- terraform/modules/gsa/main.tf
+++ terraform/modules/gsa/main.tf
@@ -37,3 +37,22 @@ resource "google_service_account_iam_member" "image_compressor_wi" {
   role               = "roles/iam.workloadIdentityUser"
   member             = "serviceAccount:${var.project_id}.svc.id.goog[imagecompressor/sa]" # hard-coded namespace eh?
 }
+
+# external secret
+resource "google_service_account" "eso_gsa" {
+    account_id = "eso-gsa"
+    display_name = "eso-gsa"
+}
+
+resource "google_project_iam_member" "eso_secret_access" {
+  project = var.project_id
+  role    = "roles/secretmanager.secretAccessor"
+  member  = "serviceAccount:${google_service_account.eso_gsa.email}"
+}
+
+resource "google_service_account_iam_member" "eso_wi" {
+  service_account_id = google_service_account.eso_gsa.name
+  role               = "roles/iam.workloadIdentityUser"
+
+  member = "serviceAccount:${var.project_id}.svc.id.goog[eso/sa]"
+}
