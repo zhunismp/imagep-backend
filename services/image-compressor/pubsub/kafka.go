fe9bf4f0aab3b0474b6c8f5023b9aac168480f8f
zhunismp <zhunismp@sensei.com>
2026-01-31 18:18:27 +0700

    chore: move dev topic creation to docker compose

--- services/image-compressor/pubsub/kafka.go
+++ services/image-compressor/pubsub/kafka.go
@@ -1,11 +1,6 @@
 package pubsub
 
 import (
-	"context"
-	"errors"
-	"fmt"
-	"time"
-
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	"github.com/zhunismp/imagep-backend/services/image-compressor/config"
 )
@@ -37,38 +32,5 @@ func NewKafkaConsumer(cfg config.Config) (*kafka.Consumer, error) {
 		return nil, err
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	if err := prepareTopic(ctx, cfg.Environment, cfg.KafkaAddress); err != nil {
-		return nil, err
-	}
-
 	return consumer, nil
-}
-
-func prepareTopic(ctx context.Context, env string, kafkaAddr string) error {
-
-	if env != "development" {
-		return nil
-	}
-
-	admin, err := kafka.NewAdminClient(&kafka.ConfigMap{"bootstrap.servers": kafkaAddr})
-	if err != nil {
-		return fmt.Errorf("error while creating admin: %v", err)
-	}
-
-	res, err := admin.CreateTopics(ctx, []kafka.TopicSpecification{{
-		Topic:             "process-image",
-		NumPartitions:     3,
-		ReplicationFactor: 1, // 1 node for dev
-	}})
-
-	for _, r := range res {
-		if r.Error.Code() != kafka.ErrNoError {
-			errors.Join(err, fmt.Errorf("result for topic %s: %v", r.Topic, r.Error))
-		}
-	}
-
-	return err
 }
